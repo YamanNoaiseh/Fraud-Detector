1ed49cb43bc0a380780c2d59ff4f35c5ab37df0c
Yaman Noaiseh <yanoaiseh@gmail.com>
2018-04-27 08:51:54 -0400

    Update producer.py

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,1 +1,24 @@
-#
+# Kafka python producer
+ 
+from kafka.client import KafkaClient
+from kafka.producer import KafkaProducer as kp
+import json
+ 
+producer = kp(bootstrap_servers=['localhost:9092'],value_serializer = lambda
+
+def produce_messages():
+    with open('locations.csv', 'r') as f:
+        first = True
+        for line in f:
+            if not first:
+                line = json.dumps(line)
+                line.strip()
+                print("produced {}".format(line))
+                producer.send('testtopic', line)
+                producer.flush()
+            else:
+                first = False
+
+if __name__ == '__main__':
+    produce_messages()
+ 
