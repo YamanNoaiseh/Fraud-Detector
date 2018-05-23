b867eefb0ff90ca112d377b386a1e939efad164b
Yaman Noaiseh <yanoaiseh@gmail.com>
2018-05-22 22:47:17 -0400

    Update fraud_producer.py

--- stream/fraud_producer.py
+++ stream/fraud_producer.py
@@ -15,8 +15,10 @@ class FraudProducer:
     producer = KafkaProducer(bootstrap_servers=['ec2-35-171-168-73.compute-1.amazonaws.com:9092',
                                                 'ec2-34-193-154-78.compute-1.amazonaws.com:9092',
                                                 'ec2-18-205-124-122.compute-1.amazonaws.com:9092',
-                                                'ec2-18-204-40-198.compute-1.amazonaws.com:9092'], 
-                                                value_serializer = lambda v: json.dumps(v).encode('utf-8'))
+                                                'ec2-18-204-40-198.compute-1.amazonaws.com:9092'
+                                                ], 
+                                                value_serializer = lambda v: json.dumps(v).encode('utf-8')
+                            )
     
     def __init__(self, fraud_topic):
         self.fraud_topic = fraud_topic
